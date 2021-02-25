067b7d57b9ae388b1f1db58514b8b4890deb55b5
Masayuki <soy2.san@gmail.com>
2021-02-25 19:04:43 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 山口雅之 on 2021/02/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
